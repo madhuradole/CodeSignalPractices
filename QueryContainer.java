f0dcbee78873b859caf5c176e15821b70b84b75c
Madhura Dole <madhuradole@gmail.com>
2025-04-20 11:47:00 -0700

    Create QueryContainer.java

---
+++ QueryContainer.java
@@ -0,0 +1,27 @@
+String[] solution(String[][] queries) {
+    String[] responses = new String[queries.length];
+    List<String> addedQueries = new ArrayList<>();
+    int i = 0;
+    for (String[] query : queries) {
+        String q = query[0], numValue = query[1];
+        switch (q) {
+            case "ADD" -> { addedQueries.add(numValue); responses[i++] = ""; }
+            case "EXISTS" -> responses[i++] = addedQueries.contains(numValue) ? "true" : "false";
+            case "REMOVE" -> responses[i++] = addedQueries.remove(numValue) ? "true" : "false";
+            case "GET_NEXT" -> responses[i++] = getNextInt(addedQueries, numValue);
+        }
+    }
+    return responses;
+}
+
+String getNextInt(List<String> addedQueries, String numValue) {
+    int minInt = Integer.MIN_VALUE;
+    int prevMin = Integer.MAX_VALUE;
+    for (String curr : addedQueries) {
+        if (Integer.valueOf(curr) > Integer.valueOf(numValue)) {
+            minInt = Math.min(prevMin, Integer.valueOf(curr));
+            prevMin = minInt;
+        }
+    }
+    return minInt == Integer.MIN_VALUE ? "" : String.valueOf(minInt);
+}
